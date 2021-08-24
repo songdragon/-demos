20d745decb63ecc92795b0d9987787710feb91fc
songdragon <songdragon@zts.io>
2021-08-24 10:32:36 +0800

    Fill blanks in LICENSE

--- springboot-fastjson/src/main/java/io/zts/demo/springboot/fastjson/config/WebMvcConfigur.java
+++ springboot-fastjson/src/main/java/io/zts/demo/springboot/fastjson/config/WebMvcConfigur.java
@@ -1,16 +1,21 @@
+/**
+ *    Copyright (c) 2021 songdragon@zts.io
+ *    Demo is licensed under Mulan PSL v2.
+ *    You can use this software according to the terms and conditions of the Mulan PSL v2.
+ *    You may obtain a copy of Mulan PSL v2 at:
+ *             http://license.coscl.org.cn/MulanPSL2
+ *    THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *    See the Mulan PSL v2 for more details.
+ */
 package io.zts.demo.springboot.fastjson.config;
 
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
